42582c64443b73328ebde52d80c6240ce0c664f1
meaghan jennings <meaghanljennings@gmail.com>
2021-04-13 22:17:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,21 +5,22 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def get_page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
-   # doc.css(".post").each do |post|
-   #   course = Course.new
-  #    course.title = post.css("h2").text
-   #   course.schedule = post.css(".date").text
-   #   course.description = post.css("p").text
-  #  end
-  end
-  
+
+  # doc.css(".post").each do |post|
+  #   course = Course.new
+  #   course.title = post.css("h2").text
+  #   course.schedule = post.css(".date").text
+  #   course.description = post.css("p").text
+  # end
+  # end
+end 
+
   def get_courses
     self.get_page.css(".post")
-  end 
+  end
 
   def make_courses
     self.get_courses.each do |post|
@@ -29,7 +30,7 @@ class Scraper
       course.description = post.css("p").text
     end
   end
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -41,7 +42,6 @@ class Scraper
     end
   end
 
-end 
-  
-Scraper.new.print_courses
+end
 
+Scraper.new.print_courses
